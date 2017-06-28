8790e8a5aeec706aaf826365e9da44edac64e042
Kenneth McAvoy <kennethmcavoy@Kenneths-MacBook-Pro.local>
2017-06-28 22:38:52 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# PollDaddy Hack
+
+This is pretty easy to use.  Just download the Python script, and customize the variables for what form/answer/number of votes.
+Be sure to add your tor hash password found in torrc file.  
+
+It needs Python 2.7.6. // Updated to Python 3.
+Also be sure to do pip install requests and pip install TorRequest
+
+### Disclaimer
+This script will **may not -- I have not tested it with new tor functionality** work on polls that do not allow multiple votes from one person.  The useragents and proxy settings will help try and mask your mass voting, but they will not get you around IP blocks.  If someone wants to give a shot at forking this and adding that functionality, I will be happy to merge it in.
+
+### Example
+You want to rig this poll: https://polldaddy.com/poll/9206448/ for the answer "It's a great way to keep kids in line during a crazy time of year.", and you want to vote 1000 times.  The poll_id comes from the url: <code>https://polldaddy.com/poll/<b>9206448</b>/</code>.  The answer_id comes from the looking at the source code for the associated checkbox: <code>\<input type="radio" name="PDI_answer" id="PDI_answer41930288" value="**41930288**"></code>.
+
+
+Thus, you would want the variables to be set to:
+```
+poll_id = 9206448
+answer_id = 41930288
+number_of_votes = 1000
+```
---
+++ voteOriginal(NoTor).py
@@ -0,0 +1,102 @@
+
+import requests, re, json, time, random
+requests.packages.urllib3.disable_warnings()
+
+# Created by Alex Beals
+# Last updated: February 20, 2016
+
+base_url = "https://polldaddy.com/poll/"
+redirect = ""
+
+useragents = []
+current_useragent = ""
+
+proxies = []
+current_proxy = {"http":""}
+current_proxy_num = -1
+
+
+def get_all_useragents():
+    f = open("useragent.txt", "r")
+    for line in f:
+        useragents.append(line.rstrip('\n').rstrip('\r'))
+    f.close()
+
+def choose_useragent():
+    k = random.randint(0, len(useragents)-1)
+    current_useragent = useragents[k]
+    #print current_useragent
+
+def get_all_proxies():
+    f = open("proxy.txt", "r")
+    for line in f:
+        proxies.append(line.rstrip('\n').rstrip('\r'))
+    f.close()
+
+def choose_proxy():
+    k = random.randint(0, len(proxies)-1)
+    current_num = k
+    current_proxy["http"] = proxies[k]
+
+
+def vote_once(form, value):
+    c = requests.Session()
+    #Chooses useragent randomly
+    choose_useragent()
+    redirect = {"Referer": base_url + str(form) + "/", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8", "User-Agent": current_useragent, "Upgrade-Insecure-Requests":"1", "Accept-Encoding": "gzip, deflate, sdch", "Accept-Language": "en-US,en;q=0.8"}
+    
+    # Chooses proxy randomly
+    choose_proxy()
+    try:
+        init = c.get(base_url + str(form) + "/", headers=redirect, verify=False, proxies=current_proxy)
+    except:
+        print("error with proxy")
+        #proxies.remove(current_proxy_num)
+        return None
+
+    # Search for the data-vote JSON object
+    data = re.search("data-vote=\"(.*?)\"",init.text).group(1).replace('&quot;','"')
+    data = json.loads(data)
+    # Search for the hidden form value
+    pz = re.search("type='hidden' name='pz' value='(.*?)'",init.text).group(1)
+    # Build the GET url to vote
+    request = "https://polldaddy.com/vote.php?va=" + str(data['at']) + "&pt=0&r=0&p=" + str(form) + "&a=" + str(value) + "%2C&o=&t=" + str(data['t']) + "&token=" + str(data['n']) + "&pz=" + str(pz)
+    try:
+        send = c.get(request, headers=redirect, verify=False, proxies=current_proxy)
+    except:
+        print("error with proxy")
+        #proxies.remove(current_proxy_num)
+        return None
+    
+    return ("revoted" in send.url)
+
+def vote(form, value, times, wait_min = None, wait_max = None):
+    global redirect
+    # For each voting attempt
+    for i in range(1, times+1):
+        b = vote_once(form, value)
+        # If successful, print that out, else try waiting for 60 seconds (rate limiting)
+        if not b:
+            # Randomize timing if set
+            if wait_min and wait_max:
+                seconds = random.randint(wait_min, wait_max)
+            else:
+                seconds = 3
+
+            print("Voted (time number " + str(i) + ")!")
+            time.sleep(seconds)
+        else:
+            print("Locked.  Sleeping for 60 seconds.")
+            i-=1
+            time.sleep(60)
+
+# Initialize these to the specific form and how often you want to vote
+poll_id = 9776576
+answer_id = 44718992
+number_of_votes = 500
+wait_min = None
+wait_max = None
+
+get_all_proxies()
+get_all_useragents()
+vote(poll_id, answer_id, number_of_votes, wait_min, wait_max)
